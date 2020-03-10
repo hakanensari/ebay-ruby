24ec4e10608f6b27f4743d73ccb273696986e9b6
Hakan Ensari <hakanensari@gmail.com>
2020-03-10 16:37:24 +0000

    Refactor config

--- lib/ebay/config.rb
+++ lib/ebay/config.rb
@@ -2,16 +2,18 @@
 
 module Ebay
   module Config
-    class << self
-      %i(app_id dev_id cert_id).each do |method|
-        eval <<-DEF, binding, __FILE__, __LINE__ + 1
+    %i[app_id dev_id cert_id].each do |method|
+      eval <<-DEF, binding, __FILE__, __LINE__ + 1
+        class << self
           attr_writer :#{method}
 
           def #{method}
             @#{method} || ENV['EBAY_#{method.upcase}']
           end
-        DEF
-      end
+        end
+
+        @#{method} = nil
+      DEF
     end
   end
 end
