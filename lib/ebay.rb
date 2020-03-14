ed2035c62e69d1186cf27dded8dce77eea1504f0
Hakan Ensari <hakanensari@gmail.com>
2020-03-14 20:04:16 +0000

    Refactor config

--- lib/ebay.rb
+++ lib/ebay.rb
@@ -1,13 +1,5 @@
 # frozen_string_literal: true
 
-require 'ebay/config'
 require 'ebay/finding'
 require 'ebay/merchandising'
 require 'ebay/shopping'
-
-# Ruby wrapper to the eBay APIs
-module Ebay
-  def self.configure
-    yield Config
-  end
-end
--- lib/ebay/config.rb
+++ lib/ebay/config.rb
@@ -1,20 +1,40 @@
 # frozen_string_literal: true
 
+# Ruby wrapper to the eBay APIs
 module Ebay
+  # Configures credentials for accessing the eBay APIs
+  # @yield {Config}
+  def self.configure
+    yield Config
+  end
+
   # Stores credentials for accessing the eBay APIs
   module Config
-    %i[app_id dev_id cert_id].each do |method|
-      eval <<-DEF, binding, __FILE__, __LINE__ + 1
-        class << self
+    class << self
+      # @!attribute [rw] app_id
+      # @return [String] unique identifier for the application
+      # @note This attribute defaults to the `EBAY_APP_ID` environment variable.
+
+      # @!attribute [rw] dev_id
+      # @return [String] unique identifier for the developer's account
+      # @note This attribute defaults to the `EBAY_DEV_ID` environment variable.
+
+      # @!attribute [rw] cert_id
+      # @return [String] certificate that authenticates the application when
+      #   making API calls
+      # @note This attribute defaults to the `EBAY_CERT_ID` environment
+      #   variable.
+      %i[app_id dev_id cert_id].each do |method|
+        eval <<-DEF, binding, __FILE__, __LINE__ + 1
           attr_writer :#{method}
 
           def #{method}
             @#{method} || ENV['EBAY_#{method.upcase}']
           end
-        end
 
-        @#{method} = nil
-      DEF
+          @#{method} = nil
+        DEF
+      end
     end
   end
 end
