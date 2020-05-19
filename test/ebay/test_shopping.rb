77b063ae74a63be2365e1a90fe0cb18ccefbcec7
Hakan Ensari <hakanensari@gmail.com>
2020-05-19 18:17:14 +0100

    Improve tests

--- test/ebay/test_shopping.rb
+++ test/ebay/test_shopping.rb
@@ -6,7 +6,7 @@ require 'ebay/shopping'
 module Ebay
   class TestShopping < Minitest::Test
     def setup
-      VCR.insert_cassette('shopping', record: :new_episodes)
+      VCR.insert_cassette('shopping')
       @request = Ebay.shopping(response_encoding: 'JSON').sandbox
     end
 
@@ -16,43 +16,50 @@ module Ebay
 
     def test_find_products
       response = @request.find_products('QueryKeywords' => 'tolkien')
-      assert response.status.ok?
+      assert_success response
     end
 
     def test_get_category_info
       response = @request.get_category_info('267')
-      assert response.status.ok?
+      assert_success response
     end
 
     def test_get_ebay_time
       response = @request.get_ebay_time
-      assert response.status.ok?
+      assert_success response
     end
 
     def test_get_item_status
-      response = @request.get_item_status('333112555211')
-      assert response.status.ok?
+      response = @request.get_item_status('291914595965')
+      assert_success response
     end
 
     def test_get_multiple_items
-      response = @request.get_multiple_items('333112555211', '322539683928')
-      assert response.status.ok?
+      response = @request.get_multiple_items('291914595965', '141963466041')
+      assert_success response
     end
 
     def test_get_shipping_costs
       response = @request.get_shipping_costs('333112555211',
                                              'DestinationCountryCode' => 'GB')
-      assert response.status.ok?
+      assert_success response
     end
 
     def test_get_single_item
       response = @request.get_single_item('333112555211')
-      assert response.status.ok?
+      assert_success response
     end
 
     def test_get_user_profile
       response = @request.get_user_profile('shopspell')
+      assert_success response
+    end
+
+    private
+
+    def assert_success(response)
       assert response.status.ok?
+      assert_equal 'Success', JSON.parse(response.body)['Ack']
     end
   end
 end
