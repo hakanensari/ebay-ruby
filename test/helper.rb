d0d8a07de7a32cdfdb29cf129d207306d38a9846
Hakan Ensari <me@hakanensari.com>
2022-10-18 17:41:50 +0100

    Remove redundant config option

--- test/helper.rb
+++ test/helper.rb
@@ -9,8 +9,7 @@ ENV['EBAY_CERT_ID'] ||= 'CERT_ID'
 VCR.configure do |c|
   c.cassette_library_dir = "#{File.dirname(__FILE__)}/cassettes"
   c.default_cassette_options = { match_requests_on: %i[uri body],
-                                 record: ENV['RECORD'] ? :once : :none,
-                                 record_on_error: true }
+                                 record: ENV['RECORD'] ? :once : :none }
   c.hook_into :webmock
   c.filter_sensitive_data('APP_ID') { ENV.fetch('EBAY_APP_ID', nil) }
   c.filter_sensitive_data('CREDENTIALS') do |interaction|
